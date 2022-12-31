3d51b142cbc84ac381122d3b0f61439692dd050a
liturner <turnertech@outlook.de>
2022-12-31 14:57:36 +0100

    Start to fix ci build for modules

---
+++ fred-jlink/install/Frederick.cmd
@@ -0,0 +1,13 @@
+:: Frederick aims to make the use of Java as transparent
+:: as possible, without resorting to platform dependent
+:: compilations.
+::
+:: We will always use shortcuts installed in the start menu
+:: and desktop with icons to this cmd.
+::
+:: START    - Runs without keeping CMD window open
+:: %~dp0    - Is the directory holding this file
+:: /B       - Should skip making any cmd windows
+::
+:: Set VM Options in the maven config for jlink!
+START "Frederick" /B "%~dp0jre\bin\javaw" -Xmx128m --module-path "%~dp0lib" --module "de.turnertech.frederick/de.turnertech.frederick.main.Application" %*
