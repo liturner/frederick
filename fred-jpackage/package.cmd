5bac9e802750d43ce786c8f28f7136663ea1baf4
Luke Ian Turner <turnertech@outlook.de>
2023-01-07 19:15:15 +0100

    Integrate jpackage into package phase (#10)
    Closes #9

--- fred-jpackage/package.cmd
+++
@@ -1,2 +0,0 @@
-REM https://docs.oracle.com/en/java/javase/14/docs/specs/man/jpackage.html
-jpackage -n Frederick -i fred-jpackage/target/dependency --main-class de.turnertech.frederick.main.Application --main-jar fred-main-1.0.1-SNAPSHOT.jar -d fred-jpackage/target --vendor TurnerTech --icon fred-jpackage/frederick-logo.ico --license-file EULA.txt --win-shortcut --win-menu
