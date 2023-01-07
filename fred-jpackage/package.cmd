f9bb936fd9971393e5a0f3be2cce31c03e527a1a
liturner <turnertech@outlook.de>
2023-01-07 15:41:41 +0100

    Minor refactoring

--- fred-jpackage/package.cmd
+++ fred-jpackage/package.cmd
@@ -1,2 +1,2 @@
 REM https://docs.oracle.com/en/java/javase/14/docs/specs/man/jpackage.html
-jpackage -n Frederick -i fred-jlink/target/dependency --main-class de.turnertech.frederick.main.Application --main-jar fred-main-1.0.0.jar -d fred-jpackage/target --vendor TurnerTech --icon fred-jpackage/frederick-logo.ico --license-file EULA.txt --win-shortcut --win-menu
+jpackage -n Frederick -i fred-jpackage/target/dependency --main-class de.turnertech.frederick.main.Application --main-jar fred-main-1.0.0.jar -d fred-jpackage/target --vendor TurnerTech --icon fred-jpackage/frederick-logo.ico --license-file EULA.txt --win-shortcut --win-menu
